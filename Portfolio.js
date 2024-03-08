637280a9f6fd1ec897d1dbcf27b0a7c8e1d7fb67
mostafa <mostafaehab.healer@gmail.com>
2024-03-09 00:15:50 +0200

    Edit and some of change

--- Portfolio.js
+++ Portfolio.js
@@ -1,7 +1,10 @@
 let section_Languages = document.getElementById("Languages") ;
 let span = document.querySelectorAll(".progress span");
 
+let up = document.querySelector('.up') ;
 window.onscroll = function () {
+    this.scrollY >= 300 ? up.classList.add("show") : up.classList.remove("show");
+
     if(window.scrollY >= section_Languages.offsetTop -200 ) {
         span.forEach( (span) => {
             span.style.width = span.dataset.width ;
@@ -10,3 +13,10 @@ window.onscroll = function () {
         })
     }
 };
+
+up.onclick=function (){
+    window.scrollTo({
+        top:0,
+        behavior:"smooth" ,
+    });
+}
